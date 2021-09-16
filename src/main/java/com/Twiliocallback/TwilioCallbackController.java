bd4c524ee537258debee54159f9771cc6b647d31
kawsikan <kawsinote@gmail.com>
2021-09-16 17:49:07 +0530

    implement action url

--- src/main/java/com/Twiliocallback/TwilioCallbackController.java
+++ src/main/java/com/Twiliocallback/TwilioCallbackController.java
@@ -2,6 +2,7 @@ package com.Twiliocallback;
 
 import com.twilio.twiml.TwiMLException;
 import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Redirect;
 import com.twilio.twiml.voice.Say;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/callback")
 public class TwilioCallbackController {
 
-    @PostMapping("")
+    @PostMapping("/gather")
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         VoiceResponse.Builder builder = new VoiceResponse.Builder();
@@ -31,10 +31,14 @@ public class TwilioCallbackController {
                     builder.say(new Say.Builder("You need support. We will help!").build());
                     break;
                 default:
-                    builder.say(new Say.Builder("Sorry, I don\'t understand that choice.").build());
+                    builder.say(new Say.Builder("Sorry, I do understand that choice.").build());
+                    builder.redirect(new Redirect.Builder("/voice").build());
                     break;
             }
+        } else {
+            builder.redirect(new Redirect.Builder("/voice").build());
         }
+
         response.setContentType("application/xml");
         try {
             response.getWriter().print(builder.build().toXml());
@@ -43,9 +47,10 @@ public class TwilioCallbackController {
         }
     }
 
-    @GetMapping("")
+    @GetMapping("/gather")
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        // Create a TwiML response and add our friendly message.
         VoiceResponse.Builder builder = new VoiceResponse.Builder();
 
         String digits = request.getParameter("Digits");
@@ -58,10 +63,14 @@ public class TwilioCallbackController {
                     builder.say(new Say.Builder("You need support. We will help!").build());
                     break;
                 default:
-                    builder.say(new Say.Builder("Sorry, I don\'t understand that choice.").build());
+                    builder.say(new Say.Builder("Sorry, I do understand that choice.").build());
+                    builder.redirect(new Redirect.Builder("/voice").build());
                     break;
             }
+        } else {
+            builder.redirect(new Redirect.Builder("/voice").build());
         }
+
         response.setContentType("application/xml");
         try {
             response.getWriter().print(builder.build().toXml());
